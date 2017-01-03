7dac449711b8e1e4d52a327ae968aa61c6912b3a
fcojgodoy <francisco.godoy@openmailbox.org>
2017-01-03 17:59:47 +0100

    first commit

---
+++ site/config/environments/development.php
@@ -0,0 +1,12 @@
+<?php
+/* Configure proxy Server */
+define('WP_PROXY_HOST', 'proxy.esperanca.guifi.net');
+define('WP_PROXY_PORT', '3128');
+define('WP_PROXY_USERNAME', 'ako.sama');
+define('WP_PROXY_PASSWORD', 'dellitognZ5');
+define('WP_PROXY_BYPASS_HOSTS', 'localhost');
+
+/** Development */
+define('SAVEQUERIES', true);
+define('WP_DEBUG', true);
+define('SCRIPT_DEBUG', true);
