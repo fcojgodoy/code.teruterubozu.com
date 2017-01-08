33d62a3718dce812c84622942d09aaf128527dd9
fcojgodoy <francisco.godoy@openmailbox.org>
2017-01-08 18:10:39 +0100

    removed dynamic_sidebar calls

--- site/web/app/themes/teruterubozu/lib/setup.php
+++ site/web/app/themes/teruterubozu/lib/setup.php
@@ -53,7 +53,7 @@ add_action('after_setup_theme', __NAMESPACE__ . '\\setup');
 /**
  * Register sidebars
  */
-function widgets_init() {
+// function widgets_init() {
 //   register_sidebar([
 //     'name'          => __('Primary', 'sage'),
 //     'id'            => 'sidebar-primary',
@@ -71,8 +71,8 @@ function widgets_init() {
 //     'before_title'  => '<h3>',
 //     'after_title'   => '</h3>'
 //   ]);
-}
-add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
+// }
+// add_action('widgets_init', __NAMESPACE__ . '\\widgets_init');
 
 /**
  * Determine which pages should NOT display the sidebar
