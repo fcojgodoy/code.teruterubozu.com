02c23cff60ff15d4f12715e0d00e3e134c0cc515
fcojgodoy <francisco.godoy@openmailbox.org>
2017-01-25 17:53:40 +0100

    improved call to the author meta location

--- site/web/app/themes/teruterubozu/templates/content-single.php
+++ site/web/app/themes/teruterubozu/templates/content-single.php
@@ -24,7 +24,12 @@
           <?php echo get_the_author_meta( 'description' ); ?>
         </p>
 
-        <?php if (get_the_author_meta('city')) : ?>
+        <!--
+        - Detect plugin Bozu User Contact Method
+        - Check if there is a 'location' author meta
+        - Show 'location' author meta -->
+        <?php include_once( ABSPATH . 'wp-admin/includes/plugin.php' ); ?>
+        <?php if ( get_the_author_meta( 'location' ) && is_plugin_active( 'bozu-user-contact-methods/bozu-user-contact-methods.php' ) ) : ?>
           <em class="author-location icon-location"><?php echo get_the_author_meta( 'city' ); ?></em>
         <?php endif ; ?>
 
